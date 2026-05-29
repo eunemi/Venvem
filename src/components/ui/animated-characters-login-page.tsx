d1914b00428af785802ef2fd138ae302d30f3ad8
Arya Yadav <240184030+eunemi@users.noreply.github.com>
2026-05-29 09:09:04 +0530

    Continue with LinkedIn Remove

--- src/components/ui/animated-characters-login-page.tsx
+++ src/components/ui/animated-characters-login-page.tsx
@@ -682,19 +682,7 @@ function LoginPage() {
               Continue with Google
             </button>
 
-            <button
-              type="button"
-              disabled={!termsAccepted}
-              className={cn(
-                "w-full h-[52px] bg-[#222222] border border-white/5 shadow-[0_4px_0_0_#050505] hover:bg-[#2a2a2a] active:shadow-none active:translate-y-[4px] text-white/90 font-medium transition-all rounded-xl flex items-center justify-center gap-3 text-[15px] outline-none",
-                !termsAccepted && "opacity-50 pointer-events-none"
-              )}
-            >
-              <svg className="size-5 text-white/70" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
-              </svg>
-              Continue with LinkedIn
-            </button>
+
           </div>
 
           {/* Sign Up Link */}
